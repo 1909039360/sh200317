c7bad1eb440d5f1122eafa001948b4a3cbbbcff2
doubleZ <1909039360@qq.com>
2020-08-19 18:34:46 +0800

    2020-08-19 18:34 second

---
+++ gmallrealtime/src/main/scala/com/atguigu/bean/EventLog.scala
@@ -0,0 +1,21 @@
+package com.atguigu.bean
+
+/**
+ * Author: doubleZ
+ * Datetime:2020/8/19   18:33
+ * Description:
+ */
+case class EventLog(mid:String,
+                    uid:String,
+                    appid:String,
+                    area:String,
+                    os:String,
+                    `type`:String,
+                    evid:String,
+                    pgid:String,
+                    npgid:String,
+                    itemid:String,
+                    var logDate:String,
+                    var logHour:String,
+                    var ts:Long)
+
